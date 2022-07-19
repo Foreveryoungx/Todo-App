4b00bfd28cd484ff83d29bb742ffb9c0ef1786de
Trey Allen <trevon.allen1994@gmail.com>
2022-07-18 23:57:53 -0500

    Update baseURL

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3030" || "https://todoserver0.herokuapp.com",
+  baseURL: "https://todoserver0.herokuapp.com",
 });
